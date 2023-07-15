b819556e69ca76e8fbaaf1f9dc6a1b6cd0d9305f
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2023-07-15 00:59:00 -0500

    Update system_setup.sh

--- system_setup.sh
+++ system_setup.sh
@@ -3,27 +3,102 @@ apt update && apt upgrade -y
 apt install sudo wget gnupg -y
 wget -q -O- https://debian.koha-community.org/koha/gpg.asc | sudo apt-key add -
 apt update
-echo 'deb http://debian.koha-community.org/koha oldstable main' | sudo tee /etc/apt/sources.list.d/koha.list
+echo 'deb http://debian.koha-community.org/koha stable main' | sudo tee /etc/apt/sources.list.d/koha.list
 apt update
-apt install koha-common -y
-apt install mariadb-server -y
+apt install koha-common mariadb-server -y
+
 # Edit the /etc/koha/koha-sites.conf as follows
-# INTRAPORT="8000"
-# OPACPORT="8001"
-# DOMAIN="Your-desired-domain
-##### This should be updated to use 443 and a self-signed cert in the future
+# INTRAPORT="80"
+# INTRASUFFIX="-admin"
+# OPACPORT="80"
+# DOMAIN=".your.desired.domain"
+cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/koha-ssl.conf
+a2enmod ssl
+a2dissite default-ssl.conf
+a2ensite koha-ssl.conf
 a2enmod rewrite
 a2enmod cgi
 service apache2 restart
-# Edit the /etc/apache2/ports.conf file as follows
-# Listen 8000
-# Listen 8001
-service apache2 restart
-koha-create --create-db new-waverly-library
+
+
+
+
+
+
+
+koha-create --create-db koha
 a2enmod headers proxy_http
-koha-plack --enable new-waverly-library
-koha-plack --start new-waverly-library
+koha-plack --enable koha
+koha-plack --start koha
 service apache2 restart
+
+
+
+
+
+
+# Edit the /etc/apache2/sites-available/koha-ssl.conf as follows
+# OPAC
+
+# <VirtualHost *:443>
+# 	<IfVersion >= 2.4>
+# 		Define instance "koha"
+# 	</IfVersion>
+# 	Include /etc/koha/apache-shared.conf
+# 	#  Include /etc/koha/apache-shared-disable.conf
+# 	Include /etc/koha/apache-shared-opac-plack.conf
+# 	Include /etc/koha/apache-shared-opac.conf
+
+# 	ServerName koha.your.dns.name
+# 	SetEnv KOHA_CONF "/etc/koha/sites/koha/koha-conf.xml"
+# 	AssignUserID koha-koha koha-koha
+
+# 	ErrorLog    /var/log/koha/koha/opac-error.log
+# 	#  TransferLog /var/log/koha/koha/opac-access.log
+
+# 	SSLEngine on
+# 	SSLCertificateFile      /etc/ssl/certs/ssl-cert-snakeoil.pem
+# 	SSLCertificateKeyFile   /etc/ssl/private/ssl-cert-snakeoil.key
+# 	<FilesMatch "\.(?:cgi|shtml|phtml|php)$">
+# 		SSLOptions +StdEnvVars
+# 	</FilesMatch>
+# 	<Directory /usr/lib/cgi-bin>
+# 		SSLOptions +StdEnvVars
+# 	</Directory>
+# </VirtualHost>
+
+# # Intranet
+# <VirtualHost *:443>
+#   <IfVersion >= 2.4>
+#    Define instance "koha"
+#   </IfVersion>
+# 	Include /etc/koha/apache-shared.conf
+# 	#  Include /etc/koha/apache-shared-disable.conf
+# 	Include /etc/koha/apache-shared-intranet-plack.conf
+# 	Include /etc/koha/apache-shared-intranet.conf
+
+# 	ServerName koha-admin.your.dns.name
+# 	SetEnv KOHA_CONF "/etc/koha/sites/koha/koha-conf.xml"
+# 	AssignUserID koha-koha koha-koha
+
+# 	ErrorLog    /var/log/koha/koha/intranet-error.log
+# 	#  TransferLog /var/log/koha/koha/intranet-access.log
+
+# 	SSLEngine on
+# 	SSLCertificateFile      /etc/ssl/certs/ssl-cert-snakeoil.pem
+# 	SSLCertificateKeyFile   /etc/ssl/private/ssl-cert-snakeoil.key
+# 	<FilesMatch "\.(?:cgi|shtml|phtml|php)$">
+# 		SSLOptions +StdEnvVars
+# 	</FilesMatch>
+# 	<Directory /usr/lib/cgi-bin>
+# 		SSLOptions +StdEnvVars
+# 	</Directory>
+# </VirtualHost>
+
+
+
+
+
 # View your koha root passwd
-koha-passwd new-waverly-library
-# Go to the INTRAPORT web UI and log in with `koha_new-waverly-library`:`<revealed-password>`
+koha-passwd koha
+# Go to the INTRAPORT web UI and log in with `koha_koha`:`<revealed-password>`
